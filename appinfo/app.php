c80172d24370c138ae63a12daea9ec7592b8edd8
Michael Usher <michael.usher@aarnet.edu.au>
2021-12-07 21:09:23 +1100

    Remove CSP policy as we aren't loading external URLs

--- appinfo/app.php
+++ appinfo/app.php
@@ -16,85 +16,7 @@ namespace OCA\Music\App;
 
 use \OCP\AppFramework\IAppContainer;
 
-$app = \OC::$server->query(Music::class);
-
-$c = $app->getContainer();
-$appName = $c->query('AppName');
-
-/**
- * add navigation
- */
-\OC::$server->getNavigationManager()->add(function () use ($c, $appName) {
-	return [
-		'id' => $appName,
-		'order' => 10,
-		'name' => $c->query('L10N')->t('Music'),
-		'href' => $c->query('URLGenerator')->linkToRoute('music.page.index'),
-		'icon' => \OCA\Music\Utility\HtmlUtil::getSvgPath('music')
-	];
-});
-
-/**
- * register regular tasks
- */
-\OC::$server->getJobList()->add('OC\BackgroundJob\Legacy\RegularJob', ['OCA\Music\Backgroundjob\Cleanup', 'run']);
-\OC::$server->getJobList()->add('OC\BackgroundJob\Legacy\RegularJob', ['OCA\Music\Backgroundjob\PodcastUpdateCheck', 'run']);
-
-/**
- * register hooks
- */
-$c->query('FileHooks')->register();
-$c->query('ShareHooks')->register();
-$c->query('UserHooks')->register();
-
-/**
- * register search provider
- */
-$c->getServer()->getSearch()->registerProvider(
-		'OCA\Music\Search\Provider',
-		['app' => $appName, 'apps' => ['files']]
-);
-
-/**
- * Set content security policy to allow streaming media from the configured external sources
- */
-function adjustCsp(IAppContainer $container) {
-	/** @var \OCP\IConfig $config */
-	$config = $container->query('Config');
-	$radioSources = $config->getSystemValue('music.allowed_radio_src', ['http://*:*', 'https://*:*']);
-	$radioHlsSources = $config->getSystemValue('music.allowed_radio_hls_src', []);
-
-	if (\is_string($radioSources)) {
-		$radioSources = [$radioSources];
-	}
-	if (\is_string($radioHlsSources)) {
-		$radioHlsSources = [$radioHlsSources];
-	}
-
-	if (!empty($radioSources) || !empty($radioHlsSources)) {
-		$policy = new \OCP\AppFramework\Http\ContentSecurityPolicy();
-
-		foreach ($radioSources as $source) {
-			$policy->addAllowedMediaDomain($source);
-		}
-
-		foreach ($radioHlsSources as $source) {
-			$policy->addAllowedConnectDomain($source);
-		}
-
-		// Also the media sources data: and blob: are needed if there are any allowed HLS sources
-		if (!empty($radioHlsSources)) {
-			$policy->addAllowedMediaDomain('data:');
-			$policy->addAllowedMediaDomain('blob:');
-		}
-
-		// Allow loading (podcast cover) images from external sources
-		$policy->addAllowedImageDomain('http://*:*');
-		$policy->addAllowedImageDomain('https://*:*');
-
-		$container->getServer()->getContentSecurityPolicyManager()->addDefaultPolicy($policy);
-	}
-}
+$appName = 'music';
 
 /**
  * Load embedded music player for Files and Sharing apps
@@ -121,11 +43,8 @@ if (isset($request->server['REQUEST_URI'])) {
 	$isMusicUrl = \preg_match('%/apps/music(/.*)?%', $url);
 
 	if ($isFilesUrl) {
-		adjustCsp($c);
 		loadEmbeddedMusicPlayer();
 	} elseif ($isShareUrl) {
 		loadEmbeddedMusicPlayer();
-	} elseif ($isMusicUrl) {
-		adjustCsp($c);
 	}
 }
